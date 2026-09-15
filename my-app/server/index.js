efa92ea8d8c124823a3904eb971919897fd77865
Lance <lance.tapang@uleth.ca>
2026-09-15 13:51:03 -0600

    uncomment

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-// const i2c = require("i2c-bus");
-// const { Gpio } = require("onoff");
+const i2c = require("i2c-bus");
+const { Gpio } = require("onoff");
 const db = require("./database");
 const { chat, listModels } = require("./llm");
 
